bbbeffa3c941e9ba036ff0d4e7e7ecfbbe37da37
ManavPresonal2001 <manavpersonal00@gmail.com>
2025-05-23 15:16:16 +0530

    Feat: CommitLint is added

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,23 +5,10 @@ const Configuration = {
 
 		'type-case': [2, 'always', 'pascal-case'],
 
-		'type-colon-space': [2, 'always'],
+		// 'type-colon-space': [2, 'always'],
 
 		'subject-case': [2, 'always', ['sentence-case']],
 	},
-	plugins: [
-		{
-			rules: {
-				'type-colon-space': ({ raw }: { raw: string }) => {
-					const match = /^([A-Z][a-z]+):\s/.test(raw);
-					return [
-						match,
-						'type must be PascalCase, immediately followed by colon, and one space after colon',
-					];
-				},
-			},
-		},
-	],
 };
 
 //check commit
