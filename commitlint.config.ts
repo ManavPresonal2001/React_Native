4a8e05c730d1936a3c29a3c4cc64e6a77ecb6e0c
ManavPresonal2001 <manavpersonal00@gmail.com>
2025-05-23 15:25:27 +0530

    teat-CommitLintIsSadded And

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -4,7 +4,7 @@ const Configuration = {
 	rules: {
 		'type-enum': [2, 'always', ['Feat', 'Fix', 'Chore']],
 		'type-case': [2, 'always', 'pascal-case'],
-		// 'type-colon-space': [2, 'always'],
+		'type-colon-space': [2, 'always'],
 		'subject-case': [2, 'always', ['sentence-case']],
 	},
 };
