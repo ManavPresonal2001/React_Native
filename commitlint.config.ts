f44058d7e0fdb9857024083f878c08a4c664a4cb
ManavPresonal2001 <manavpersonal00@gmail.com>
2025-05-23 15:04:49 +0530

    Feat: commitLint is added

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -2,8 +2,11 @@
 const Configuration = {
 	rules: {
 		'type-enum': [2, 'always', ['Feat', 'Fix', 'Chore']],
+
 		'type-case': [2, 'always', 'pascal-case'],
+
 		'type-colon-space': [2, 'always'],
+
 		'subject-case': [2, 'always', ['sentence-case']],
 	},
 	//check commit check again
