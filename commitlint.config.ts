77d31109a509bf03ac76aec3b86d3369a15aab16
ManavPresonal2001 <manavpersonal00@gmail.com>
2025-05-23 12:50:12 +0530

    Feat: Integrate Track property feature

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,10 +1,32 @@
 /** @type {import('@commitlint/types').UserConfig} */
-const Configuration: import('@commitlint/types').UserConfig = {
+const Configuration = {
 	rules: {
+		// Only allow these types
 		'type-enum': [2, 'always', ['Feat', 'Fix', 'Chore']],
+
+		// Enforce PascalCase type (Feat, Fix, etc.)
 		'type-case': [2, 'always', 'pascal-case'],
-		'subject-case': [0],
+
+		// No space before colon and exactly one space after
+		'type-colon-space': [2, 'always'],
+
+		// Capitalize first letter of subject
+		'subject-case': [2, 'always', ['sentence-case']],
 	},
+
+	plugins: [
+		{
+			rules: {
+				'type-colon-space': ({ raw }: { raw: string }) => {
+					const match = /^([A-Z][a-z]+):\s/.test(raw);
+					return [
+						match,
+						'type must be PascalCase, immediately followed by colon, and one space after colon',
+					];
+				},
+			},
+		},
+	],
 };
 
 module.exports = Configuration;
