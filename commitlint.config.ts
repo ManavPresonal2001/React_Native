19a3c1885b7f1080e3e55f3c0edd0b8f143ca134
ManavPresonal2001 <manavpersonal00@gmail.com>
2025-05-23 15:27:44 +0530

    Feat: CommitLintIsSadded And

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -7,8 +7,20 @@ const Configuration = {
 		'type-colon-space': [2, 'always'],
 		'subject-case': [2, 'always', ['sentence-case']],
 	},
-};
 
-//check commit
+	plugins: [
+		{
+			rules: {
+				'type-colon-space': ({ raw }: { raw: string }) => {
+					const match = /^([A-Z][a-z]+):\s/.test(raw);
+					return [
+						match,
+						'type must be PascalCase, immediately followed by colon, and one space after colon',
+					];
+				},
+			},
+		},
+	],
+};
 
 module.exports = Configuration;
